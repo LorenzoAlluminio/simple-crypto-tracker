4c1eca45c6d667634b46e6a7adf844a73a828e58
Lorenzo Alluminio <alluminio.lorenzo@gmail.com>
2021-10-10 18:40:06 +0200

    part of txledger implementation

---
+++ txledger/tx.py
@@ -0,0 +1,34 @@
+class Tx:
+    def __init__(self,platform,coin,tx_id,status,timestamp,from_addr,to_addr,amount,fees,token_txs,gas_price,coin_price):
+        self.platform = platform
+        self.coin = coin
+        self.tx_id = tx_id
+        self.status = status
+        self.timestamp = timestamp
+        self.from_addr = from_addr
+        self.to_addr = to_addr
+        self.amount = amount
+        self.fees = fees
+        self.gas_price = gas_price
+        self.coin_price = coin_price
+        # list of TokenTxs
+        # TODO maybe check that it is actually a list of TokenTx
+        self.token_txs = token_txs
+    
+    def to_string(self):
+        for token_tx in self.token_txs:
+            if token_txs_string is None:
+                token_txs_string += token_tx.to_string()
+            else: 
+                token_txs_string += ", " + token_tx.to_string()
+        return f"platform: {self.platform}, coin: {self.coin}, tx_id: {self.tx_id}, status: {self.status}, timestamp: {self.timestamp}, from_addr: {self.from_addr}, to_addr: {self.to_addr}, amount: {self.amount}, fees: {self.fees}, gas_price: {gas_price}, coin_price: {coin_price}, tokenTxs: {token_txs_string}"
+
+class TokenTx:
+    def __init__(self,token,from_addr,to_addr,amount):
+        self.token = token
+        self.from_addr = from_addr
+        self.to_addr = to_addr
+        self.amount = amount
+
+    def to_string(self):
+        return f"(token: {self.token}, from_addr: {self.from_addr}, to_addr: {self.to_addr}, amount: {self.amount})"
---
+++ txledger/txledger.py
@@ -0,0 +1,34 @@
+import os
+import shelve
+
+class TxLedger: 
+    # initializes ledger from a json database / creates it from scratch
+    def __init__(self,database_name):
+        self.db = shelve.open(database_name)
+        self.database_name = database_name
+
+    # destructor that closes the db (not sure if it's really useful or not)
+    def __del__(self):
+        self.db.close()
+    
+    # add a transaction to the db
+    def put_tx(self,platform,tx_id,tx):
+        self.db[platform+tx_id] = tx
+
+    # retrieve a transaction from the db
+    def get_tx(self,platform,tx_id):
+        key = platform + tx_id
+        if key in self.db:
+            return self.db[key]
+        else:
+            return None
+    
+    # print database name
+    def print_database_name(self):
+        print("filename " + self.database_name)
+    
+    # print database content
+    def print_db(self):
+        for key in self.db.keys():
+            print(key + " - " + self.db[key])
+
