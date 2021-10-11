1ef803bf9816772157660d37942c7da59246048a
Lorenzo Alluminio <alluminio.lorenzo@gmail.com>
2021-10-11 18:12:47 +0200

    make db hierarchical by platform

--- txledger/txledger.py
+++ txledger/txledger.py
@@ -4,7 +4,7 @@ import shelve
 class TxLedger: 
     # initializes ledger from a json database / creates it from scratch
     def __init__(self,database_name):
-        self.db = shelve.open(database_name)
+        self.db = shelve.open(database_name, writeback=True)
         self.database_name = database_name
 
     # destructor that closes the db (not sure if it's really useful or not)
@@ -12,14 +12,17 @@ class TxLedger:
         self.db.close()
     
     # add a transaction to the db
+    # TODO add check for already existing transaction
     def put_tx(self,tx):
-        self.db[tx.platform+tx.tx_id] = tx
+        if tx.platform not in self.db:
+            self.db[tx.platform] = {}
+        self.db[tx.platform][tx.tx_id] = tx
 
     # retrieve a transaction from the db
+    #TODO raise an error instead of return None? to consider
     def get_tx(self,platform,tx_id):
-        key = platform + tx_id
-        if key in self.db:
-            return self.db[key]
+        if platform in self.db and tx_id in self.db[platform]:
+            return self.db[platform][tx_id]
         else:
             return None
     
@@ -29,6 +32,10 @@ class TxLedger:
     
     # print database content
     def print_db(self):
-        for key in self.db.keys():
-            print(key + " - " + self.db[key].to_string())
+        for platform in self.db.keys():
+            for tx_id in self.db[platform].keys():
+                print(f"({platform},{tx_id}) --> {self.db[platform][tx_id].to_string()}")
 
+    # removes the entire content of the database
+    def clear_db(self):
+        self.db.clear()
